48138bdc5545cbb75fa46150a3b6db7d3bf38ffe
Jair Gomez <jairherber505@gmail.com>
2023-07-13 15:43:17 -0700

    working on the btn functioality

---
+++ public/assets/js/dash.js
@@ -0,0 +1,8 @@
+// * Getting the card post to display
+document.getElementById("new-post").addEventListener("click", function () {
+  document.getElementById("create-card").style.display = "block";
+});
+// * If cancel hide the create post card
+document.getElementById("cancel-btn").addEventListener("click", function () {
+  document.getElementById("create-card").style.display = "none";
+});
